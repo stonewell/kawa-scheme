227147417cc50978d26d74baf2e4632c908082d0
Per Bothner <per@bothner.com>
2017-07-15 13:23:37 +0200

    * kawa/arglist.scm: Allow general sequence.

--- kawa/lib/kawa/arglist.scm
+++ kawa/lib/kawa/arglist.scm
@@ -5,24 +5,24 @@
 
 (define-procedure arglist-key-count
   (lambda (args ::gnu.mapping.ArgList)::int (args:numKeywords))
-  (lambda (args ::java.util.List)::int 0))
+  (lambda (args ::sequence)::int 0))
 
 (define-procedure arglist-key-start
   (lambda (args ::gnu.mapping.ArgList)::int (args:firstKeyword))
-  (lambda (args ::java.util.List)::int (args:size)))
+  (lambda (args ::sequence)::int (args:size)))
 
 (define-procedure arglist-arg-count
   (lambda (x ::gnu.mapping.ArgList)::int (x:numArguments))
-  (lambda (x ::java.util.List)::int (x:size)))
+  (lambda (x ::sequence)::int (x:size)))
 
 (define-procedure arglist-key-ref
   (lambda (x ::gnu.mapping.ArgList i::int) (x:getKeyword i))
-  (lambda (x ::java.util.List i::int)
+  (lambda (x ::sequence i::int)
     (primitive-throw (java.lang.IndexOutOfBoundsException))))
 
 (define-procedure arglist-arg-ref
   (lambda (x ::gnu.mapping.ArgList i::int) (x:getArgAsObject i))
-  (lambda (x ::java.util.List i::int) (x:get i)))
+  (lambda (x ::sequence i::int) (x:get i)))
 
 (define-procedure arglist-walk
   (lambda (args::gnu.mapping.ArgList proc)::void
@@ -31,13 +31,13 @@
                 (key0 (args:firstKeyword)))
             (do ((i ::int 0 (+ i 1))) ((= i nargs))
               (proc (args:getKeyword i) (args:getArgAsObject i)))))
-  (lambda (args ::java.util.List proc)
+  (lambda (args ::sequence proc)
     (for-each (lambda (v) (proc #!null v)) args)))
 
 (define-procedure arglist-key-index
   (lambda (args ::gnu.mapping.ArgList key)::int
           (args:findKeyword key))
-  (lambda (args ::java.util.List key)::int
+  (lambda (args ::sequence key)::int
     -1))
 
 (define (arglist-key-value args key default)
